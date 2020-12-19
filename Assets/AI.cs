ab290989d087df1d2cef3f3392d3b38cda2a20e9
abdulhadi602 <ahadi3608@gmail.com>
2020-12-19 18:30:00 +0000

    Doin with joystick

--- Assets/AI.cs
+++ Assets/AI.cs
@@ -6,28 +6,46 @@ public class AI : MonoBehaviour
 {
     private Transform Player;
     public float speed;
-
+    private float posy;
+    private float currentPercentage;
+    private float currentdist;
     void Start()
     {
         Player = GameObject.FindGameObjectWithTag("Player").transform;
-        speed = 0.0029f;
-   ;
+        speed = 0.0059f;
+        posy = 0;
     }
 
     private void LookAwayFrom(Vector3 point)
     {
         point = 2.0f * transform.position - point;
         // point = new Vector3(point.x, point.y, point.z);
-        point.z = point.x;
+        
         transform.LookAt(point );
-        transform.position = Vector3.Lerp(transform.position, new Vector3(point.x,point.y,0), speed);
-     
+        transform.position = Vector3.Lerp(transform.position, new Vector3(point.x,0,point.z), speed);
+        transform.Translate(new Vector3(point.x, posy, point.z)*speed*Time.deltaTime);
     }
   
     private void FixedUpdate()
     {
         LookAwayFrom(Player.position);
-       
+        currentdist = Vector3.Distance(transform.position, Player.position);
+        if (currentdist < 2)
+        {
+            posy += (currentdist / 50) * Time.deltaTime;
+            speed -= Time.deltaTime;
+        }
+        else
+        {
+            if (posy > 0)
+            {
+                posy -= (currentdist / 2) * Time.deltaTime;
+                if(speed< 0.0059f)
+                {
+                    speed += Time.deltaTime;
+                }
+            }
+        }
     }
-   
+  
 }
